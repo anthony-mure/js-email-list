49a3e38b589695e0fe8c7fad05f780b9c4bbd380
anthony-murè <tony.mure99@gmail.com>
2025-07-18 17:01:10 +0200

    [add]generate-list-email

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@
 https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista.*/
 
+//recupero gli elementi dal dom che mi serviranno
+
+const emailList = document.getElementById('list');
+
+//creo una variabile con un array vuoto dove metterò al suo interno gli elementi generati
+
+const generateEmail = [];
+
+//uso un ciclo for per generare 10 elementi 
+//dentro al ciclo effettuo una chiamata ajax con l'apposita API
+//pusho gli elementi generati nell'array vuoto
+//innietto la variabile con l'array di 10 elementi nell'html
+
+for(let i=0 ; i < 10 ; i++){
+
+  axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
+
+      generateEmail.push(resp.data.response);
+
+      emailList.innerHTML = `<li>${generateEmail}</li>`;
+  });
+};
